73f5d6b489c4b496e21204cb3d8a79cecdfd84b8
kebean <chenqiua@gmail.com>
2023-08-17 21:05:55 +0300

    function that will render a book

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const btn = document.querySelector("#btn");
 const form = document.querySelector("#book-form");
+const bookLibrary = document.querySelector("#books-library");
 btn.addEventListener("click", () =>{
  form.style.display = "block";
 })
@@ -17,9 +18,26 @@ function addBookToLibrary(){
     const pages =  document.querySelector("#pages").value;
     const read =  document.querySelector("#read").checked;
     const book = new Book(title, author, pages, read);
-    console.log( book)
+    library.push(book);
+    showBook()
 }
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     addBookToLibrary()
 })
+function showBook(){
+    bookLibrary.innerHTML = "";
+    for (let i = 0; i < library.length; i++){
+        let book = library[i];
+        let bookElement = document.createElement("div")
+        bookElement.innerHTML= `
+        <h3>${book.title}<h3>
+        by
+        <h4>${book.author}<h4>
+        has
+        <p>${book.pages}<p>
+        <p>${book.read? "read": "not read yet"}<p>
+        `
+        bookLibrary.appendChild(bookElement);
+    }
+}
