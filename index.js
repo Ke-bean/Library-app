e6419726eb7d22e98d511ed9c0a478a6e3c7a383
kebean <chenqiua@gmail.com>
2023-08-17 20:43:56 +0300

    a class Object Book that creat Book instances

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const form = document.querySelector("#book-form");
 btn.addEventListener("click", () =>{
  form.style.display = "block";
 })
+
+let library = [];
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
